75a262c84e1d42d3501ff141d113a4da436238de
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-05-10 02:35:03 -0500

    feat: update Tailwind CSS dependencies to version 4.1.6 and enhance calculator button functionality
    - Updated @tailwindcss/postcss and tailwindcss to version 4.1.6 in package.json and package-lock.json.
    - Enhanced calculator button styles with cursor pointer and pressed state.
    - Modified calculator button HTML to include click event handling.
    - Refactored calculator button component TypeScript to manage button press states and emit click events.
    - Updated calculator component to handle keyboard events and integrate with the calculator service.
    - Implemented a new CalculatorService to manage calculator logic and state.

--- 01-zoneless-calculator/src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ 01-zoneless-calculator/src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,4 +1,4 @@
-import { booleanAttribute, ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
+import { booleanAttribute, ChangeDetectionStrategy, Component, type ElementRef, HostBinding, input, output, signal, viewChild } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -7,7 +7,8 @@ import { booleanAttribute, ChangeDetectionStrategy, Component, HostBinding, inpu
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './calculator-button.component.css',
   host: {
-    class: ' border-r border-b border-indigo-400',
+    class: ' border-r border-b border-indigo-400 w-1/4',
+    '[class]': 'isDoubleSize() ? "w-2/4" : "w-1/4"',
     attribute: 'hola',
     'data-size': 'XL'
   },
@@ -15,7 +16,9 @@ import { booleanAttribute, ChangeDetectionStrategy, Component, HostBinding, inpu
  */})
 export class CalculatorButtonComponent {
 
-
+  isPressed = signal(false)
+  onClick = output<string>()
+  contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
   isCommand = input(false, { transform: booleanAttribute });
   isDoubleSize = input(false, { transform: booleanAttribute });
 
@@ -23,8 +26,30 @@ export class CalculatorButtonComponent {
      return this.isCommand();
    } */
 
-  @HostBinding('class.w-2/4') get commandStyle() {
-    return this.isDoubleSize() ? 'w-2/4' : 'w-1/4';
+  /*   @HostBinding('class.w-2/4') get commandStyle() {
+      return this.isDoubleSize() ? 'w-2/4' : 'w-1/4';
+    } */
+
+  handleClick() {
+    if (!this.contentValue()?.nativeElement) { return }
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    const value = this.contentValue()!.nativeElement.innerText
+    this.onClick.emit(value.trim())
+  }
+
+  keyboardPressedStyle(key: string) {
+    if (!this.contentValue()) { return }
+
+    const value = this.contentValue()?.nativeElement.innerHTML
+    if (value !== key) return;
+
+    this.isPressed.set(true)
+
+    setTimeout(() => {
+
+      this.isPressed.set(false)
+    }, 100)
+
   }
 
 }
--- 01-zoneless-calculator/src/app/calculator/components/calculator/calculator.component.ts
+++ 01-zoneless-calculator/src/app/calculator/components/calculator/calculator.component.ts
@@ -1,10 +1,57 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, viewChildren } from '@angular/core';
 import { CalculatorButtonComponent } from "../calculator-button/calculator-button.component";
+import { CalculatorService } from '@/calculator/services/calculator.service';
 
 @Component({
   selector: 'calculator',
   imports: [CalculatorButtonComponent],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)'
+  }
 })
-export class CalculatorComponent { }
+export class CalculatorComponent {
+
+  private calculatorService = inject(CalculatorService)
+
+  calculatorButtons = viewChildren(CalculatorButtonComponent)
+
+  public resultText = computed(() => this.calculatorService.resultText())
+  public subResultText = computed(() => this.calculatorService.subResultText())
+  public lastOperator = computed(() => this.calculatorService.lastOperator())
+
+  /*   get resultTex(){
+      return this.calculatorService.resultText
+    }
+   */
+
+  handleClick(key: string) {
+    console.log(key)
+    this.calculatorService.constructorNumber(key)
+  }
+
+
+  /*   @HostListener('document:keyup', ['$event'])
+   */
+  handleKeyboardEvent(event: KeyboardEvent) {
+
+    const keyEquivalent: Record<string, string> = {
+      Escape: 'C',
+      Clear: 'C',
+      X: '*',
+      '/': '÷',
+      Enter: '='
+    }
+
+    const key = event.key
+    const keyValue = keyEquivalent[key] ?? key
+
+
+    this.handleClick(keyValue)
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    this.calculatorButtons().forEach(button => {
+      button.keyboardPressedStyle(keyValue)
+    })
+  }
+}
